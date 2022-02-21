b38958e9cb31ca50ef1c2a2e7552c93f7a4c9f78
Kyungsuh Park <pyo05360@gmail.com>
2022-02-21 23:27:29 +0900

    모달창 띄우기

---
+++ 모달창/main.js
@@ -0,0 +1,7 @@
+const btnModal = document.querySelector(".btn-modal");
+const modal = document.querySelector(".cont-modal");
+
+
+btnModal.addEventListener('click', () => {
+    modal.style.display = "block";
+})
