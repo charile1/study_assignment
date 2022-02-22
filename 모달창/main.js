2612b4978d3e2ec1f7e2edae6a6649db8ec5531d
Kyungsuh Park <pyo05360@gmail.com>
2022-02-22 21:29:54 +0900

    모달창 기능 넣기

--- 모달창/main.js
+++ 모달창/main.js
@@ -1,7 +1,27 @@
 const btnModal = document.querySelector(".btn-modal");
-const modal = document.querySelector(".cont-modal");
+const modal = document.querySelector(".modal-overlay");
+const btnClose = document.querySelector(".btn-close");
 
 
 btnModal.addEventListener('click', () => {
-    modal.style.display = "block";
+    modal.style.display = "flex"
+})
+
+btnClose.addEventListener('click',() => {
+    modal.style.display = "none"
+})
+
+//오버레이 클릭 시 창 꺼지기 
+modal.addEventListener('click', (i) => {
+    const eventTarget= i.target;
+    if(eventTarget.classList.contains("modal-overlay")) {
+        modal.style.display = "none"
+    }
+})
+
+// esc 버튼 눌러서 창 꺼지기
+window.addEventListener("keyup", (i) => {
+    if(modal.style.display === "flex" && i.key === "Escape") {
+        modal.style.display = "none"
+    }
 })
